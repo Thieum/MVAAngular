638deb9e2691bbd6dbde13df71fcb03f4361945b
Matthieu Penant <thieum22@hotmail.com>
2016-04-15 11:57:22 -0400

    typos dependency / ies

--- 02-Controllers/01-CreatingAController/app/mainApp.js
+++ 02-Controllers/01-CreatingAController/app/mainApp.js
@@ -1,5 +1,5 @@
-/*Angular Modules take a name, best practice is lowerCamelCase, and a list of dependancies*/
-/*added the second module as a dependancy */
+/*Angular Modules take a name, best practice is lowerCamelCase, and a list of dependencies*/
+/*added the second module as a dependency */
 angular.module('mainApp', ['eventModule'])
 .config([function () {
 	/* Configuration is where you configure providers ( not instances)*/
